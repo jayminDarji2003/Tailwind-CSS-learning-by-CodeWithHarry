b944b1de961fd71d07018b9c0752d05fc0967ad2
Jaymin <jaymindarji2003@gmail.com>
2023-06-21 11:44:09 +0530

    Lecture 5 done

---
+++ Lecture 4/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    fontFamily: {
+      'time' : ['Times, Times New Roman, serif'],
+    },
+  },
+  plugins: [],
+}
+
